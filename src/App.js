c07836996c13fef476eaae2ca1b110694245f4b9
Simon Atkins <simon@flickapp.com>
2021-03-11 11:04:03 +0000

    Install react Styled and add basic styling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import styled from "styled-components";
+import { device } from "./constants/deviceSizes";
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+  max-width: 30%;
+  margin: auto;
+  @media ${device.mobile} {
+    max-width: 90%;
+  }
+`;
+
+const StyledButton = styled.button`
+  align-self: flex-end;
+  margin: 1em;
+`;
 
 const initialTodos = [
-    {
-        id: 'a',
-        task: 'Do dishes',
-        complete: false
-    },
-    {
-        id: 'b',
-        task: 'Paint walls',
-        complete: false
-    },
-    {
-        id: 'c',
-        task: 'Fix car',
-        complete: false
-    },
-]
-
+  {
+    id: "a",
+    task: "Do dishes",
+    complete: false,
+  },
+  {
+    id: "b",
+    task: "Paint walls",
+    complete: false,
+  },
+  {
+    id: "c",
+    task: "Fix car",
+    complete: false,
+  },
+];
 
 const App = () => {
-    return (
-        <div className="App">
-            <ul>
-                {initialTodos.map(todo =>
-                    <li key={todo.id}>
-                        <label>{todo.task}</label>
-                    </li>
-                )}
-            </ul>
-        </div>
-    );
-}
+  const [task, updateTask] = useState();
+
+  const handleChangeInput = (event) => {
+    updateTask(event.target.value);
+  };
+
+  console.log("task?", task);
+
+  return (
+    <StyledApp className="App">
+      <h1>To Do List</h1>
+      <input onChange={handleChangeInput} />
+      <StyledButton type={"submit"}>Add To Do Item</StyledButton>
+      <ul>
+        {initialTodos.map((todo) => (
+          <li key={todo.id}>
+            <label>{todo.task}</label>
+          </li>
+        ))}
+      </ul>
+    </StyledApp>
+  );
+};
 
 export default App;
---
+++ src/constants/deviceSizes.jsx
@@ -0,0 +1,11 @@
+const size = {
+    mobile: '425px',
+    tablet: '768px',
+    desktop: '1024px',
+}
+
+export const device = {
+    mobile: `(min-width: ${size.mobile})`,
+    tablet: `(min-width: ${size.tablet})`,
+    desktopL: `(min-width: ${size.desktop})`
+};
