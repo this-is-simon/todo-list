e066e6e4f50695ac31f12fbe24bbe5071e24fa5c
Simon Atkins <simon@flickapp.com>
2021-03-11 10:55:22 +0000

    Add initial todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialTodos = [
+    {
+        id: 'a',
+        task: 'Do dishes',
+        complete: false
+    },
+    {
+        id: 'b',
+        task: 'Paint walls',
+        complete: false
+    },
+    {
+        id: 'c',
+        task: 'Fix car',
+        complete: false
+    },
+]
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <ul>
+                {initialTodos.map(todo =>
+                    <li key={todo.id}>
+                        <label>{todo.task}</label>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
